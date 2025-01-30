9ee7b33dec7346a11fb97b00c54af9a6d6d96ff9
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2025-01-30 23:51:50 +0100

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import ImageModal from './components/ImageModal/ImageModal';
+import Loader from './components/Loader/Loader';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import SearchBar from './components/SearchBar/SearchBar';
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <LoadMoreBtn />
+      <SearchBar />
+      <ImageGallery />
+      <Loader />
+      <ErrorMessage />
+      <ImageModal />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import ImageCard from '../ImageCard/ImageCard';
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <ImageCard />
+      <ImageCard />
+      <ImageCard />
+    </div>
+  );
+};
+
+export default ImageGallery;
